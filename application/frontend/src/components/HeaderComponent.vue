574204db09910d866a90d93aed28058332710458
churikova-olga <olga_churikova890@mail.ru>
2022-10-14 21:01:54 +0300

    050102 / Исправлен компонент HeaderComponent.vue vue.js
    -добавлена стилизация элементов
    -исправлены эелементы

--- application/frontend/src/components/HeaderComponent.vue
+++ application/frontend/src/components/HeaderComponent.vue
@@ -18,17 +18,17 @@
     </div>
 
     <div class="top_header_logotype">
-      <span class="logotype">LOGOTYPE</span>
+      <img class="logotype" :src="require('../assets/icon/iconLogo.svg')" height="50" width="167" alt="logo"/>
     </div>
-    <div class="top_header_functional">
-        <div class="functional_bag">
-          <span class="functional_bag_price" :class="{'hidden': this.price === 0 }"> {{this.price}} ₽</span>
-          <img class="top_header_icon" :src="require('../assets/icon/iconBag.svg')" height="30" width="30" alt="bag"/>
-        </div>
-        <img class="top_header_icon" :src="require('../assets/icon/iconHeart.svg')" height="30" width="30" alt="heart"/>
-        <img class="top_header_icon" :src="require('../assets/icon/iconUser.svg')" height="30" width="30" alt="user"/>
+
+    <div class="top_header_nav">
+      <ul class="nav_list">
+        <li class="nav_element"><a href="#" class="nav_element_link">Акции</a></li>
+        <li class="nav_element"><a href="#" class="nav_element_link">О нас</a></li>
+        <li class="nav_element"><a href="#" class="nav_element_link">Помощь</a></li>
+        <li class="nav_element"><a href="#" class="nav_element_link">Контакты</a></li>
+      </ul>
     </div>
-    <img class="top_header_icon top_header_menu" :src="require('../assets/icon/iconMenu.svg')" height="30px" width="30px" alt="menu"/>
   </div>
 
   <div class="contacts_social">
@@ -36,8 +36,6 @@
       <a href="https://www.whatsapp.com/" class="social_networks social_networks_margin">WhatsApp</a>
       <a href="https://web.telegram.org/" class="social_networks">Telegram</a>
   </div>
-<!--    <span class="contacts_text media_contacts"> +7(999)999-99-99 </span>-->
-<!--    <span class="contacts_text media_contacts">BuyGuitars@mail.com</span>-->
 
 </div>
 <div class="bottom_header">
@@ -51,17 +49,19 @@
     <div class="bottom_header_search">
       <img :src="require('../assets/icon/iconSearch.svg')" height="15" width="15" alt="search"/>
       <input class="search_input" placeholder="Поиск..."/>
+      <button class="search_button"> <img :src="require('../assets/icon/iconArrow.svg')" alt="arrow" width="24" height="14"> </button>
     </div>
     </div>
 
-    <div class="bottom_header_nav">
-      <ul class="nav_list">
-        <li class="nav_element"><a href="#" class="nav_element_link">Акции</a></li>
-        <li class="nav_element"><a href="#" class="nav_element_link">О нас</a></li>
-        <li class="nav_element"><a href="#" class="nav_element_link">Помощь</a></li>
-        <li class="nav_element"><a href="#" class="nav_element_link">Контакты</a></li>
-      </ul>
+    <div class="bottom_header_functional">
+      <div class="functional_bag">
+        <span class="functional_bag_price" :class="{'hidden': this.price === 0 }"> {{this.price}} ₽</span>
+        <img class="top_header_icon" :src="require('../assets/icon/iconBag.svg')" height="30" width="30" alt="bag"/>
+      </div>
+      <img class="top_header_icon" :src="require('../assets/icon/iconHeart.svg')" height="30" width="30" alt="heart"/>
+      <img class="top_header_icon" :src="require('../assets/icon/iconUser.svg')" height="30" width="30" alt="user"/>
     </div>
+<!--    <img class="top_header_icon top_header_menu" :src="require('../assets/icon/iconMenu.svg')" height="30px" width="30px" alt="menu"/>-->
   </div>
 </div>
 </div>
@@ -70,10 +70,33 @@
 <script>
 export default {
   name: "HeaderComponent",
-  data(){
-    return{
+  data() {
+    return {
       price: 15550,
+      a: null, b: null
     }
+  },
+  methods: {
+    Sticky: function () {
+      this.b = document.querySelector('.top_header');
+      this.a = document.querySelector('.bottom_header');
+      console.log(this.b.getBoundingClientRect().top)
+      if(this.b.getBoundingClientRect().top >= -70)
+      {
+        this.a.classList.remove('sticky');
+      }
+      else if (this.a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+        this.a.classList.add('sticky');
+      }
+    },
+  },
+  created: function (){
+    window.addEventListener('scroll', this.Sticky, false);
+    document.body.addEventListener('scroll', this.Sticky, false);  // если у html и body высота равна 100%
+  },
+  destroyed: function () {
+    window.addEventListener('scroll', this.Sticky, false);
+    document.body.addEventListener('scroll', this.Sticky, false);  // если у html и body высота равна 100%
   }
 }
 </script>
@@ -81,18 +104,17 @@ export default {
 <style scoped>
 
 .header{
-  padding: 0 7%;
+  padding: 1.7% 0 0;
+  background-color: rgba(29, 29, 29, 1);
+  color: rgba(248, 248, 248, 1);
 }
 /*Верхняя часть хедера*/
-.top_header{
+.top_header {
   min-height: 67px;
-  margin-top: 40px;
-  margin-bottom: 25px;
-  /*background-color: navajowhite;*/
   display: flex;
-  border-bottom: 1px solid black;
+  border-bottom: 1px solid rgba(248, 248, 248, 1);
   flex-direction: column;
-
+  margin: 0 7%;
 }
 .top_header_main {
   height: 50%;
@@ -101,10 +123,11 @@ export default {
   cursor: pointer;
   margin: 5px;
 }
-.main{
+.main {
   display: flex;
   justify-content: space-between;
-  height: 50%;
+  /*height: 50%;*/
+  align-items: center;
   width: 100%;
 }
 .contacts_social {
@@ -127,14 +150,13 @@ export default {
   font-size: 14px;
   font-weight: 500;
   margin-left: 5px;
-  color: black;
   white-space: nowrap;
 }
 .social_networks {
   font-size: 12px;
   font-weight: 400;
+  color: rgba(248, 248, 248, 1);
   text-decoration: none;
-  color: black;
 }
 .social_networks:hover {
   color: #2b669a;
@@ -150,10 +172,10 @@ export default {
   margin-left: 10px
 }
 
-.top_header_functional {
+.bottom_header_functional {
   display: flex;
   align-items: center;
-  width: 20%;
+  width: 24%;
   justify-content: space-between;
 
 }
@@ -170,28 +192,41 @@ export default {
 }
 .top_header_logotype  {
   position: absolute;
-  margin-top: 5px;
-  left: 50%;
-  margin-bottom: 3px;
+  margin-top: -11px;
+  left: 45%;
   display: flex;
   align-items: center;
 }
 
-.logotype {
-  font-weight: 900;
-  font-size: 20px;
-}
 .hidden{
   visibility: hidden;
 }
 /*Нижняя часть хедера*/
+
+.bottom_header {
+  padding: 0 7%;
+  min-height: 73px;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  z-index: 101;
+  width: 86%;
+  background-color: rgba(29, 29, 29, 1);;
+  /*top: 93.5px;*/
+}
+
+.sticky{
+  top: 0;
+  position: fixed;
+  z-index: 101;
+}
 .bottom_header_catalog {
   display: flex;
   align-items: center;
 }
-.bottom_header_nav {
+.top_header_nav {
   display: flex;
-  width: 38%;
+  width: 33%;
 }
 .nav_list {
   margin: 0;
@@ -208,21 +243,32 @@ export default {
 
 .bottom_header_search {
   display: flex;
-  border: 2px solid black;
+  border: none;
   align-items: center;
   width: 60%;
   border-radius: 11px;
   min-height: 29px;
   max-height: 35px;
-  padding: 0 10px;
+  padding-left: 10px;
   margin-left: 10px;
-  box-sizing: border-box;
+  background-color: rgba(55, 55, 55, 1);
+  /*box-sizing: border-box;*/
 }
 .search_input {
   border: none;
   width: 100%;
-  margin-left: 5px;
+  margin: 0 5px;
   outline:none;
+  background-color: rgba(55, 55, 55, 1);
+  color: rgba(248, 248, 248, 1);
+}
+.search_button{
+  background: #DFB259;
+  border-radius: 11px;
+  border: none;
+  width: 55px; /*Исправить потом*/
+  height: 29px;
+  cursor: pointer;
 }
 /*noinspection CssNoGenericFontName*/
 .catalog_title{
@@ -234,9 +280,9 @@ export default {
 /*noinspection CssNoGenericFontName*/
 .nav_element_link{
   text-decoration: none;
-  color: black;
-  font-size: 17px;
+  font-size: 15px;
   font-weight: 500;
+  color: rgba(248, 248, 248, 1);
   font-family: Inter semiBold;
 }
 .nav_element_link:hover{
@@ -251,52 +297,52 @@ export default {
 
 
 /*Медиа запросы*/
-@media screen and (max-width: 850px) {
-  .contacts_email {
-    display: none;
-  }
-  .top_header_functional {
-    display: none;
-  }
-  .bottom_header_nav{
-    display: none;
-  }
-  .top_header_menu {
-    display: flex;
-  }
-  .contacts_phone {
-    display: flex;
-  }
-  .bottom_header_wrapper{
-    width: 100%;
-  }
-}
-@media screen and (max-width: 400px) {
-  .top_header_contacts {
-    display: none;
-  }
-  .top_header_main {
-    justify-content: flex-end;
-  }
-  .top_header_logotype {
-    left: 7%;
-  }
-}
+/*@media screen and (max-width: 850px) {*/
+/*  .contacts_email {*/
+/*    display: none;*/
+/*  }*/
+/*  .bottom_header_functional {*/
+/*    display: none;*/
+/*  }*/
+/*  .top_header_nav{*/
+/*    display: none;*/
+/*  }*/
+/*  .top_header_menu {*/
+/*    display: flex;*/
+/*  }*/
+/*  .contacts_phone {*/
+/*    display: flex;*/
+/*  }*/
+/*  .bottom_header_wrapper{*/
+/*    width: 100%;*/
+/*  }*/
+/*}*/
+/*@media screen and (max-width: 400px) {*/
+/*  .top_header_contacts {*/
+/*    display: none;*/
+/*  }*/
+/*  .top_header_main {*/
+/*    justify-content: flex-end;*/
+/*  }*/
+/*  .top_header_logotype {*/
+/*    left: 7%;*/
+/*  }*/
+/*}*/
 
-@media screen and (min-width: 850px) {
-  .contacts_email {
-    display: flex;
-  }
-  .top_header_functional {
-    display: flex;
-  }
-  .top_header_menu {
-    display: none;
-  }
-  .contacts_phone {
-    display: flex;
-  }
+/*@media screen and (min-width: 850px) {*/
+/*  .contacts_email {*/
+/*    display: flex;*/
+/*  }*/
+/*  .bottom_header_functional {*/
+/*    display: flex;*/
+/*  }*/
+/*  .top_header_menu {*/
+/*    display: none;*/
+/*  }*/
+/*  .contacts_phone {*/
+/*    display: flex;*/
+/*  }*/
 
-}
+/*}*/
 
 </style>
